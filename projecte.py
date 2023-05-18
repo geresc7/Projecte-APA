0d478c90d469ebc86e501fe30f2c8b4a5da32b3d
Gerard <gerard.escardo@estudiantat.upc.edu>
2023-05-18 16:52:15 +0200

    3

--- projecte.py
+++ projecte.py
@@ -2,7 +2,18 @@ import cv2
 import numpy as np
 from tkinter.filedialog import *
 from tkinter import *
+from PIL import ImageTk, Image
 
 
-gui = Tk(className="Interfaz") #variable tipo tkinter (interfaz)
-gui.geometry("400x500") #tamany finestra interfaz creo
+root = Tk()
+root.title("One Shot")
+root.iconbitmap("Logo.ico")
+
+my_img = ImageTk.PhotoImage(Image.open("nom_imatge.png"))
+my_label = Label (image=my_img)
+my_label.pack()
+
+button_quit = Button(root, text="Sortir del programa", command=root.quit)
+button_quit.pack()
+
+root.mainloop()

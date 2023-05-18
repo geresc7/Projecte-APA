27b9e3a63699fc9af7f3d0732ffccc3abe4fe51e
Gerard <gerard.escardo@estudiantat.upc.edu>
2023-05-18 16:52:48 +0200

    4

--- projecte.py
+++ projecte.py
@@ -13,7 +13,7 @@ my_img = ImageTk.PhotoImage(Image.open("nom_imatge.png"))
 my_label = Label (image=my_img)
 my_label.pack()
 
-button_quit = Button(root, text="Sortir del programa", command=root.quit)
+button_quit = Button(root, text="Sortir del Programa", command=root.quit)
 button_quit.pack()
 
 root.mainloop()

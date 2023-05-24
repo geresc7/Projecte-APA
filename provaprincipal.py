e448f33031d94275a979309c4b4549388adeb4cc
Gerard <gerard.escardo@estudiantat.upc.edu>
2023-05-24 18:38:11 +0200

    interface

--- provaprincipal.py
+++ provaprincipal.py
@@ -8,43 +8,56 @@ def cargar_imagen():
     ruta_imagen = askopenfilename(filetypes=[("Archivos de imagen", "*.png;*.jpg;*.jpeg")])
     if ruta_imagen:
         imagen = cv2.imread(ruta_imagen)
-        imagen_editada = edicion_imagenes.aplicar_filtro_grises(imagen)
+        imagen_editada = aplicar_funcionalidad(imagen, funcionalidad_var.get())
         mostrar_imagen(imagen_editada)
+
+def aplicar_funcionalidad(imagen, funcionalidad):
+    if funcionalidad == "Escala de grises":
+        imagen_editada = edicion_imagenes.aplicar_filtro_grises(imagen)
+    elif funcionalidad == "Desenfoque":
+        imagen_editada = edicion_imagenes.aplicar_filtro_blur(imagen)
+    # Agrega más opciones de funcionalidad según tus necesidades
+    return imagen_editada
 
 def mostrar_imagen(imagen):
     ventana = Toplevel()
     ventana.title("Imagen Editada")
+    ventana.configure(background="#F2F2F2")
+
     imagen = cv2.cvtColor(imagen, cv2.COLOR_BGR2RGB)
     imagen_pil = ImageTk.PhotoImage(Image.fromarray(imagen))
     etiqueta_imagen = Label(ventana, image=imagen_pil)
-    etiqueta_imagen.pack()
+    etiqueta_imagen.pack(pady=10)
+
     ventana.mainloop()
 
 root = Tk()
 root.title("One Shot")
 root.iconbitmap("Logo.ico")
+root.configure(background="#F2F2F2")
 
-frame_superior = Frame(root)
+frame_superior = Frame(root, bg="#F2F2F2")
 frame_superior.pack()
 
-label_titulo = Label(frame_superior, text="One Shot")
-label_titulo.config(font=("Arial", 24))
+label_titulo = Label(frame_superior, text="One Shot", font=("Arial", 24), bg="#F2F2F2")
 label_titulo.pack(pady=10)
 
-frame_imagen = Frame(root)
-frame_imagen.pack()
-
-my_img = ImageTk.PhotoImage(Image.open("nom_imatge.png"))
-my_label = Label(frame_imagen, image=my_img)
-my_label.pack(pady=10)
-
-frame_botones = Frame(root)
+frame_botones = Frame(root, bg="#F2F2F2")
 frame_botones.pack()
 
-boton_cargar = Button(frame_botones, text="Cargar Imagen", command=cargar_imagen)
-boton_cargar.pack(side=LEFT, padx=10)
+funcionalidad_var = StringVar()
+funcionalidad_var.set("Escala de grises")
 
-button_quit = Button(frame_botones, text="Salir del Programa", command=root.quit)
-button_quit.pack(side=RIGHT, padx=10)
+opcion_1 = Radiobutton(frame_botones, text="Escala de grises", variable=funcionalidad_var, value="Escala de grises", font=("Arial", 14), bg="#F2F2F2", activebackground="#F2F2F2", selectcolor="#F2F2F2")
+opcion_1.pack(side=LEFT, padx=10)
+
+opcion_2 = Radiobutton(frame_botones, text="Desenfoque", variable=funcionalidad_var, value="Desenfoque", font=("Arial", 14), bg="#F2F2F2", activebackground="#F2F2F2", selectcolor="#F2F2F2")
+opcion_2.pack(side=LEFT, padx=10)
+
+boton_cargar = Button(root, text="Cargar Imagen", command=cargar_imagen, font=("Arial", 14), relief="flat", bg="#4CAF50", fg="white", activebackground="#45A049", activeforeground="white")
+boton_cargar.pack(pady=20)
+
+button_quit = Button(root, text="Salir del Programa", command=root.quit, font=("Arial", 14), relief="flat", bg="#F44336", fg="white", activebackground="#E53935", activeforeground="white")
+button_quit.pack()
 
 root.mainloop()

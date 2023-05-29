7dbf4c2d605e8477b4c935009cf5cf09d5f3ed52
Gerard <gerard.escardo@estudiantat.upc.edu>
2023-05-29 18:16:02 +0200

    borrar

--- provaPIL.py
+++
@@ -1,7 +0,0 @@
-from PIL import Image   #llibreria PILLOW
-import os
-
-image1 = Image.open('nom_imatge.jpg') #obrir imatge
-image1.show()
-
-image1.save('nom_imatge2.png') #guarda imatge (es pot passar de jpg a png, pero no al reves)
